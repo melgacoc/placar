5df68284382cd2d8de609362c2b5a853a4e6a914
Cláudio <claudio19a@hotmail.com>
2023-03-21 14:53:10 -0300

    fixing lint

--- app/backend/src/api/controller/MatchesController.ts
+++ app/backend/src/api/controller/MatchesController.ts
@@ -39,7 +39,6 @@ export default class MatchesController {
       return res.status(422).json({ message:
             'It is not possible to create a match with two equal teams' });
     }
-
     const result = await this.matchesService.newMatch(
       homeTeamId,
       homeTeamGoals,
@@ -47,8 +46,7 @@ export default class MatchesController {
       awayTeamGoals,
     );
     if (!result) {
-      return res.status(404).json({ message:
-        'There is no team with such id!' });
+      return res.status(404).json({ message: 'There is no team with such id!' });
     }
     res.status(201).json(result);
   };
