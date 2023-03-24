07a0397d47269502c8f2fd286b4cbcd39ecb3963
Cláudio <claudio19a@hotmail.com>
2023-03-24 02:04:29 -0300

    req 23, 24 e 25

--- app/backend/src/api/interfaces/ILeaderBoard.ts
+++ app/backend/src/api/interfaces/ILeaderBoard.ts
@@ -1,12 +1,12 @@
 export default interface ILeaderBoard {
-    name: string | undefined,
-    totalPoints: number,
-    totalGames: number,
-    totalVictories: number,
-    totalDraws: number,
-    totalLosses: number,
-    goalsFavor: number,
-    goalsOwn: number,
-    goalsBalance: number,
-    efficiency: string,
-  }
+  name: string | undefined,
+  totalPoints: number,
+  totalGames: number,
+  totalVictories: number,
+  totalDraws: number,
+  totalLosses: number,
+  goalsFavor: number,
+  goalsOwn: number,
+  goalsBalance: number,
+  efficiency: string,
+}
